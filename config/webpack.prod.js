cfb74a93a8da881aa8cd0724ba937167bcd0a559
Влад <45657091+mamonovvlad@users.noreply.github.com>
2022-09-01 11:43:00 +0300

    fixed prod

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,24 +1,22 @@
 // Build config:
 const webpack = require("webpack");
-const dotenv = require("dotenv");
-dotenv.config();
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common");
-
+const dotenv = require("dotenv");
+dotenv.config();
 
 module.exports = merge(common, {
   // PRODUCTION MODE
   mode: "production",
   devtool: false,
   plugins: [
+    
     new webpack.DefinePlugin({
-      "process.env": {
-        PROXY: `'${process.env.PROXY_PROD}'`,
-        PROXY2: `'${process.env.PROXY2_PROD}'`,
-      },
+      "process.env.PROXY": `'${process.env.PROXY_PROD}'`,
+      "process.env.PROXY2": `'${process.env.PROXY2_PROD}'`,
     }),
-  ],
   
+  ],
 });
 
 
