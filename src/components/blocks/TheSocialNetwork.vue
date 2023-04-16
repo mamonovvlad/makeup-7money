acbeac7662ff06cfa20cdb27a0500c77d29e7af8
Влад <45657091+mamonovvlad@users.noreply.github.com>
2023-04-16 11:34:34 +0300

    ашÑremove link telegram

--- src/components/blocks/TheSocialNetwork.vue
+++ src/components/blocks/TheSocialNetwork.vue
@@ -1,22 +1,48 @@
 <template>
   <div class="social-network">
-    <a href="//www.youtube.com/channel/UCF9Id35NfuuZne1ao3qXbjw" target="_blank">
-      <img src="../../assets/images/social-network/youtube.svg" loading="lazy" alt="youtube" />
+    <a
+      href="//www.youtube.com/channel/UCF9Id35NfuuZne1ao3qXbjw"
+      target="_blank"
+    >
+      <img
+        src="../../assets/images/social-network/youtube.svg"
+        loading="lazy"
+        alt="youtube"
+      />
     </a>
-    <a href="//t.me/seven_money" target="_blank">
-      <img src="../../assets/images/social-network/tel.svg" loading="lazy" alt="telegram" />
+    <a href="tg://resolve?domain=seven_money" target="_blank">
+      <img
+        src="../../assets/images/social-network/tel.svg"
+        loading="lazy"
+        alt="telegram"
+      />
     </a>
     <a href="//twitter.com/7moneyCo" target="_blank">
-      <img src="../../assets/images/social-network/twit.svg" loading="lazy" alt="twitter" />
+      <img
+        src="../../assets/images/social-network/twit.svg"
+        loading="lazy"
+        alt="twitter"
+      />
     </a>
     <a href="//www.instagram.com/7money.co/" target="_blank">
-      <img src="../../assets/images/social-network/instagram.svg" alt="instagram" />
+      <img
+        src="../../assets/images/social-network/instagram.svg"
+        alt="instagram"
+      />
     </a>
     <a href="//vk.com/7money_obmenik" target="_blank">
-      <img src="../../assets/images/social-network/vk.svg" loading="lazy" alt="vk" />
+      <img
+        src="../../assets/images/social-network/vk.svg"
+        loading="lazy"
+        alt="vk"
+      />
     </a>
     <a href="//ok.ru/group7money.c" target="_blank">
-      <img src="../../assets/images/social-network/ok.svg" loading="lazy" alt="ok" />
+      <img
+        src="../../assets/images/social-network/ok.svg"
+        loading="lazy"
+        alt="ok"
+      />
     </a>
   </div>
 </template>
